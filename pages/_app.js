a3ceae1cf66fae10255f38d74c6b0a32127313c6
sushma1031 <sushmajram@gmail.com>
2023-05-25 20:54:31 +0530

    Import bootstrap css & js globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-import "bootstrap/dist/css/bootstrap.css";
+import React, { useEffect } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap");
+  }, []);
   return <Component {...pageProps} />;
 }
