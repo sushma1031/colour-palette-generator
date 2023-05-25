3a8d9cc9ecd08ef4ff4107d66def58b9c831eb2b
sushma1031 <sushmajram@gmail.com>
2023-05-25 22:45:32 +0530

    Add footer component

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer py-2">
+      <p>
+        Made with{"  "}
+        <i className="fa-solid fa-heart" style={{ color: "#f1f1f1" }}></i> by
+        Sushma Jayaram
+      </p>
+    </footer>
+  );
+}
