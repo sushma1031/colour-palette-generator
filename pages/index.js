a1b56b38a0d0d7db2fc785002f15a67d1e0c71ea
sushma1031 <sushmajram@gmail.com>
2023-05-25 20:56:12 +0530

    Reformat index.js

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,10 @@
-import Head from 'next/head';
-import Palette from '../components/Palette.js';
-import Header from '../components/Header';
+import InputBar from '../components/InputBar.js';
+import Layout from '../components/Layout.js';
 
 export default function App() {
   return (
-    <>
-      <Head>
-        <title>Colour Palette Generator</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-
-      <Header />
-
-      <footer>Made by Sushma Jayaram</footer>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </>
+      <Layout>
+        <InputBar />
+      </Layout>
   );
 }
