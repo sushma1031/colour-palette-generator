e1774a4591bddbe564fcd2cee20a3e27fb9c3a85
sushma1031 <sushmajram@gmail.com>
2023-05-25 23:07:03 +0530

    Add fontawesome script

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
+import Script from "next/script";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    require("bootstrap/dist/js/bootstrap");
+    require("bootstrap/dist/js/bootstrap.js");
   }, []);
-  return <Component {...pageProps} />;
+  return (<>
+    <Script src="https://kit.fontawesome.com/689f04e9e5.js" />
+    <Component {...pageProps} />
+  </>);
 }
