047cfd994e1a751091023981ef4c669bafba02ca
sushma1031 <sushmajram@gmail.com>
2023-05-26 18:37:03 +0530

    Revert "Update path for favicon"
    This reverts commit 21fa3f315c37819a4ff046e9f23d9e693bbd082c.

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>Colour Palette Generator</title>
-        <link rel="icon" href="../public/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="utf-8" />
       </Head>
