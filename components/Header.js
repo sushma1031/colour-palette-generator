a509dfff16a51fe4a743e498a5ae92e57ad608e2
sushma1031 <sushmajram@gmail.com>
2023-05-26 22:37:53 +0530

    Update css loading
    - global.css: Add necessary comment and remove font import
    - Header.js: Add font import using next/font/google

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Prata } from "next/font/google";
 
+const prata = Prata({ subsets: ["latin"], weight: "400", display: "swap" });
 export default function Header() {
   return (
     <header>
-      <h2 className="heading">Generate Beautiful Palettes</h2>
+      <h2 className={`${prata.className} heading`}>Generate Beautiful Palettes</h2>
     </header>
   );
 }
