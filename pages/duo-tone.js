e76f97a821a96e51ca133131831033a593343967
sushma1031 <sushmajram@gmail.com>
2023-05-26 16:39:18 +0530

    Update duo-tone to display message

--- pages/duo-tone.js
+++ pages/duo-tone.js
@@ -1,9 +1,14 @@
 import Layout from "../components/Layout";
 
 export default function App() {
+  const customStyle = {
+    textAlign: "center",
+    margin: "2rem 0"
+  };
+
   return (
       <Layout>
-        <h1>Duo Tone</h1>
+        <h4 style={customStyle}>This page is a work in progress, please check back later!</h4>
       </Layout>
 
   );
