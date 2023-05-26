93a93df8809f9d62e6aa260d14e00fb42d055179
sushma1031 <sushmajram@gmail.com>
2023-05-26 22:15:31 +0530

    Revert "Reformat css loading"
    This reverts commit ad12145871d78878155dcd87cc40103b3ad08378.

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,15 @@
 import React, { useEffect } from "react";
 import Script from "next/script";
-import Head from "next/head";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/global.css"
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.js");
   }, []);
-  return (
-    <>
-      <Head>
-        <Script src="https://kit.fontawesome.com/689f04e9e5.js" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return (<>
+    <Script src="https://kit.fontawesome.com/689f04e9e5.js" />
+    <Component {...pageProps} />
+  </>);
 }
