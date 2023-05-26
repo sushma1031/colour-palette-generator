a393bdea776b2172379cb6814ef4e4ae3be8a564
sushma1031 <sushmajram@gmail.com>
2023-05-26 16:34:05 +0530

    Import PaletteBox in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import InputBar from '../components/InputBar.js';
 import Layout from '../components/Layout.js';
+import PaletteBox from '../components/PaletteBox.js';
 
 export default function App() {
   return (
       <Layout>
-        <InputBar />
+      <InputBar />
+      <PaletteBox/>
       </Layout>
   );
 }
