9bb27663b8dac27dce24e31bfdfdfd052d71725e
sushma1031 <sushmajram@gmail.com>
2023-05-26 16:32:35 +0530

    Add PaletteBox component

---
+++ components/PaletteBox.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../styles/PaletteBox.module.css";
+import ColourBox from "./ColourBox";
+
+export default function PaletteBox(props) {
+  return (
+    <div
+      id="palette"
+      className={
+        "mx-lg-auto mb-2" +
+        (props.error ? " mt-lg-3" : " mt-lg-5") +
+        ` ${styles.paletteBox}`
+      }
+    >
+      {["000", "111", "222", "333", "444"].map((colour, index) => {
+        return <ColourBox key={index} hex={colour} />;
+      })}
+    </div>
+  );
+}
