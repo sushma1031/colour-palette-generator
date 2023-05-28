44bb66c17bb590c7faf65a8540a5d898f4827582
sushma1031 <sushmajram@gmail.com>
2023-05-28 16:30:48 +0530

    Add theme switch handlers

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,35 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import ModeNightIcon from "@mui/icons-material/ModeNight";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
 export default function Layout({ children }) {
+  const themeIcons = [<ModeNightIcon />, <LightModeIcon />];
+  const [themeIconIndex, setThemeIconIndex] = useState(0);
+  const [dark, setDark] = useState(false);
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    if (prefersDark) {
+      setDark(true);
+    }
+  }, [dark]);  
+ 
+  function handleThemeSwitch() {
+    document.querySelector("body").classList.toggle("dark");
+    setDark(!dark);
+    setThemeIconIndex((prev) => {
+      if (prev === 0)
+        return 1;
+      return 0;
+    })
+  }
+  
+
   return (
     <>
       <Head>
@@ -12,7 +39,7 @@ export default function Layout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="utf-8" />
       </Head>
-      <Navbar />
+      <Navbar handleTheme={handleThemeSwitch} icon={themeIcons[themeIconIndex]} />
       <div className="content-wrap container-fluid">
         <Header />
         <div>{children}</div>
