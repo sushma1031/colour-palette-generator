f105ba65d2ab2d4be2a7ad421e8158b34005e377
sushma1031 <sushmajram@gmail.com>
2023-08-05 18:36:55 +0530

    Handle colour selection
    Update the respective colour value in the array

--- components/InputGroup.js
+++ components/InputGroup.js
@@ -3,14 +3,22 @@ import styles from "../styles/InputGroup.module.css";
 import InputBar from "./InputBar";
 import GenerateButton from "./GenerateButton";
 
-export default function InputGroup() {
+export default function InputGroup(props) {
   const [colours, setColours] = useState([]);
+  const [error, setError] = useState(false);
+  
+  function handleChange(index, colour) {
+    setColours((prev) => {
+      prev[index] = colour;
+      return prev;
+    });
+  }
 
   return (
     <div className={`${styles.group}`}>
-      <InputBar index={0} single={false} error={""} />
-      <GenerateButton hoverEffect={true} />
-      <InputBar index={1} single={false} error={""} />
+      <InputBar index={0} single={false} error={error} changeColours={handleChange}/>
+      <GenerateButton hoverEffect={true} handleClick={() => props.handleClick(colours)} />
+      <InputBar index={1} single={false} error={error} changeColours={handleChange}/>
     </div>
   );
 }
