f8b7b1861d96a031552a90c1f54ef99dbf6308e5
sushma1031 <sushmajram@gmail.com>
2023-08-05 12:08:48 +0530

    Update InputGroup.js
    Render two input bars with a signle generate icon in between

--- components/InputGroup.js
+++ components/InputGroup.js
@@ -1,5 +1,16 @@
+import React, { useState } from "react";
 import styles from "../styles/InputGroup.module.css";
+import InputBar from "./InputBar";
+import GenerateButton from "./GenerateButton";
 
-export default function InputGroup({ children }) {
-  return <div className={`${styles.group}`}>{ children }</div>;
+export default function InputGroup() {
+  const [colours, setColours] = useState([]);
+
+  return (
+    <div className={`${styles.group}`}>
+      <InputBar index={0} single={false} />
+      <GenerateButton />
+      <InputBar index={1} single={false} />
+    </div>
+  );
 }
