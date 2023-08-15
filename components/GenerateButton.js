788e4116ad6c3036024673196b4a21a6346a683c
sushma1031 <sushmajram@gmail.com>
2023-08-15 23:42:49 +0530

    Remove hover effect for generate button

--- components/GenerateButton.js
+++ components/GenerateButton.js
@@ -3,17 +3,20 @@ import IconButton from "@mui/material/IconButton";
 import AutoFixNormalIcon from "@mui/icons-material/AutoFixNormal";
 
 export default function GenerateButton(props) {
-  const onHover = props.hoverEffect
-    ? {}
-    : {
-        "&.MuiButtonBase-root:hover": {
-          bgcolor: "transparent",
-        },
-      };
   return (
     <div aria-label="generate colour palette">
-      <IconButton type="submit" sx={onHover} onClick={props.handleSubmit}>
-        <AutoFixNormalIcon sx={{ color: "#2c2c2c" }} />
+      <IconButton
+        type="submit"
+        sx={{
+          "&.MuiButtonBase-root:hover": {
+            bgcolor: "transparent",
+          },
+        }}
+        onClick={props.handleSubmit}
+      >
+        <AutoFixNormalIcon
+          sx={{ color: "#2c2c2c", backgroundColor: "#f4f4f4" }}
+        />
       </IconButton>
     </div>
   );
