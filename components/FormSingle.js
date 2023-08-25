4c8d49ddb37e0d13f411ae8c9f248e9f5f7cc4a0
sushma1031 <sushmajram@gmail.com>
2023-08-25 11:16:03 +0530

    Create form component with styles

---
+++ components/FormSingle.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import styles from '../styles/Form.module.css'
+import InputBar from "./InputBar";
+import IconButton from "@mui/material/IconButton";
+import AutoFixNormalIcon from "@mui/icons-material/AutoFixNormal";
+import ColorizeIcon from "@mui/icons-material/Colorize";
+import ColourPicker from "./ColourPicker";
+
+export default function Form(props) {
+  const [error, setError] = useState(false);
+  const [colour, setColour] = useState("");
+  const [showColourPicker, setShowColourPicker] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setShowColourPicker(false);
+    console.log("Clicked!");
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={`col-xl-5 mx-auto px-4 ${styles.layout}`}
+    >
+      <InputBar />
+      <div className={`${styles.buttonStyle}`}>
+        <IconButton
+          type="submit"
+          sx={{
+            "&.MuiButtonBase-root:hover": {
+              bgcolor: "transparent",
+            },
+          }}
+          aria-label="generate colour palette"
+          size="medium"
+          disableRipple
+        >
+          <AutoFixNormalIcon sx={{ color: "#2c2c2c" }} fontSize="inherit" />
+        </IconButton>
+      </div>
+      <div className={`${styles.break}`}></div>
+      {true && (
+        <div id="colourHelp" className={`form-text ${styles.smallText}`}>
+          Please enter a valid HEX colour code.
+        </div>
+      )}
+    </form>
+  );
+}
