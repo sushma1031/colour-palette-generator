c07a139ced19d60b77c9641306dd58e058bb9a74
sushma1031 <sushmajram@gmail.com>
2023-09-04 00:26:56 +0530

    Remove validation from duo-tone.js

--- pages/duo-tone.js
+++ pages/duo-tone.js
@@ -1,34 +1,26 @@
 import React, { useState } from "react";
-import PaletteBox from "../components/PaletteBox";
-import Form from "../components/FormSingle";
-import Layout from "../components/Layout";
-import { generatePaletteDuo } from "../lib/colours";
-import DownloadButton from "../components/DownloadButton";
+import Form from "../src/FormDuo";
+import PaletteBox from "../src/PaletteBox";
+import Layout from "../src/Layout";
+import { duoTone } from "../lib/generatePalette";
+import DownloadButton from "../src/DownloadButton";
 
 export default function DuoTone() {
-  const initialPalette = generatePaletteDuo(["#F35969", "#ccc", "#b476f3"]);
+  const initialPalette = duoTone(["#F35969", "#CCC", "#B476F3"]);
   const [list, setList] = useState(initialPalette);
-  const [error, setError] = useState(false);
 
-  function handleClick(colours) {
+  function createPalette(colours) {
     try {
-      setError(false);
-      const testValidHex = (colour) => /^#(?:[0-9A-F]{3}){1,2}$/i.test(colour);
-      if (colours.length === 2 && colours.every(testValidHex)) {
-        const palette = generatePaletteDuo(colours);
-        console.log(palette);
-        setList(palette);
-      } else {
-        setError(true);
-      }
+      const palette = duoTone(colours);
+      setList(palette);
     } catch (err) {
       console.log(err.message);
     }
   }
   return (
     <Layout>
-      <Form />
-      <PaletteBox coloursList={list} error={error} />
+      <Form createPalette={createPalette} />
+      <PaletteBox coloursList={list} />
       <DownloadButton />
     </Layout>
   );
